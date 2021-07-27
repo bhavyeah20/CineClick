1f9e1d6753c2f83627b9f5eaab1bdb2593d6ddb3
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-07-27 11:52:56 +0530

    Done with full screen playback

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Featured from '../components/featured/Featured'
-import List from '../components/list/List'
-import Navbar from '../components/navbar/Navbar'
+import Featured from '../../components/featured/Featured'
+import List from '../../components/list/List'
+import Navbar from '../../components/navbar/Navbar'
 import "./home.scss"
 const Home = () => {
     return (
