1561f66a934585771f3574a4edbc430c9e28d20c
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-07-27 17:36:52 +0530

    Done with Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./app.scss"
 import Home from "./pages/home/Home"
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 import Watch from "./pages/watch/Watch";
 
 const App = () => {
-  return <Register/>;
+  return <Login/>;
 };
 
 export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,29 @@
+import "./login.scss"
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="top">
+                <div className="wrapper">
+                    <span className="logo">CINECLICK</span>
+                </div>
+            </div>
+            <div className="container">
+                <form>
+                    <h1>Sign In</h1>
+                    <input type="email" placeholder="Email or Phone Number" />
+                    <input type="password" placeholder="Password" />
+                    <button className="loginButton">Sign In</button>
+                    <span>New to Netflix ? <b>Sign up now.</b></span>      
+                    <small>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. <b>Learn more</b>
+                    </small>
+                </form>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login
