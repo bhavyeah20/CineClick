2bcaf0a51837c47dcd435597ed8fbdd31b2d14a7
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-10-04 12:43:35 +0530

    Minor Changes

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = "0d0f41899d83bb4005049a85998cf8ce";
 
 const requests = {
-  fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=2`,
   fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US`,
